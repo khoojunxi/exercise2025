60a985a9e1a6fa0be5a4c84dd714fc9631d53fef
Harmless_dawn <50298174+khoojunxi@users.noreply.github.com>
2025-11-11 11:14:43 +0800

    Update W05E02.java

--- Exercise2025/src/W05/W05E02.java
+++ Exercise2025/src/W05/W05E02.java
@@ -1,37 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package W05;
+import java.util.Scanner; 
 
-import java.util.Scanner;
-
-/**
- *
- * @author pensyarah
- */
 public class W05E02 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-           Scanner scanner = new Scanner(System.in);
 
+      
+        Scanner scanner = new Scanner(System.in);
         int sum = 0;
-        while (true) {
-            System.out.print("Enter a number (0 to stop): ");
-            int input = scanner.nextInt();
-            if (input == 0) {
-                break;
+        int input; 
+
+        do {
+            System.out.println("Enter a number (0 to stop ):");
+            input = scanner.nextInt();
+
+            
+            if (input != 0) {
+                sum += input;
             }
 
-            sum += input;
-
-        }
+        } while (input != 0);
 
         System.out.println("The total sum is: " + sum);
     }
-    
 }
